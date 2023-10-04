94fd4711ad1441332b768fba42b8655ab7531704
김승아 <ksa7841@gmail.com>
2023-10-04 15:33:43 +0900

    fix: Fix error

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json()); // body-parser
 
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.send('Hi');
 });
 
 app.listen(PORT, () => {
