524244ccc7ce139f5863e05befe7757ef0148e93
9woody5 <woojin958@gmail.com>
2023-10-05 15:25:37 +0900

    feat: Add public header/footer components

---
+++ src/views/pages/Cart/Cart.js
@@ -0,0 +1,13 @@
+// header 모듈 가져오기
+fetch('../../public/Header/Header.html')
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById('header').innerHTML = data;
+  });
+
+// footer 모듈 가져오기
+fetch('../../public/Footer/Footer.html')
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById('footer').innerHTML = data;
+  });
