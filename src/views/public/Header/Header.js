2f0f14d9d34c2d80122f0fb6a8e728316f924fc7
9woody5 <woojin958@gmail.com>
2023-10-06 15:58:17 +0900

    feat: Add header, footer modules

--- src/views/public/Header/Header.js
+++ src/views/public/Header/Header.js
@@ -1,5 +1,12 @@
 import { onPageLoad } from '../utils/onPageLoad.js';
 
+// header 모듈 가져오기
+fetch('../../public/Header/Header.html')
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById('header').innerHTML = data;
+  });
+
 // 페이지가 로드될 때 실행
 window.addEventListener('load', onPageLoad);
 
