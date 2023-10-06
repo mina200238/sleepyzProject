e42cb1631e2605ff6204d113e2530198832f6da2
Namhyun <jenple9709@gmail.com>
2023-10-07 00:30:43 +0900

    feat: Add Slide banner

--- src/views/pages/Main/Main.js
+++ src/views/pages/Main/Main.js
@@ -14,3 +14,29 @@ fetch('../../public/Footer/Footer.html')
   .then((data) => {
     document.getElementById('footer').innerHTML = data;
   });
+
+//배너 슬라이드 기능
+let currentSlide = 0;
+const slides = document.querySelectorAll('.main-banner .slides img');
+const totalSlides = slides.length;
+
+document.querySelector('.nextSlide').addEventListener('click', function () {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  updateSlide();
+});
+
+document.querySelector('.prevSlide').addEventListener('click', function () {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  updateSlide();
+});
+
+function updateSlide() {
+  let offset = -currentSlide * 100;
+  document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+// 자동 슬라이드 기능
+let slideInterval = setInterval(function () {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  updateSlide();
+}, 3000);
