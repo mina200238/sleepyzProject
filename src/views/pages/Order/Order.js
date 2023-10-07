28956768fdf5cf5408cde64a8b2acaa513766cc8
김승아 <ksa7841@gmail.com>
2023-10-07 15:42:52 +0900

    chore: Merge dev-front

--- src/views/pages/Order/Order.js
+++ src/views/pages/Order/Order.js
@@ -26,7 +26,7 @@
 document.getElementById('orderForm').addEventListener('submit', function (e) {
   e.preventDefault();
 
-  var formData = new FormDataEvent(this);
+  var formData = new FormData(this);
 
   for (var pair of formData.entries()) {
     console.log(pair[0 + ': ' + pair[1]]);
