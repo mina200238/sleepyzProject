5a39d7704b6f2dd3e848e4e31c5c9cf16cad5a2d
9woody5 <woojin958@gmail.com>
2023-10-08 16:00:44 +0900

    refactor: Update public styles

--- src/views/public/utils/updateBadge.js
+++ src/views/public/utils/updateBadge.js
@@ -1,6 +1,6 @@
 // 장바구니 아이콘 badge 숫자 업데이트
 export default function updateBadge(itemCount) {
-  const badgeElement = document.getElementById('cartBadge');
+  const badgeElement = document.getElementById('cart-badge');
   if (badgeElement) {
     badgeElement.textContent = itemCount.toString();
   }
