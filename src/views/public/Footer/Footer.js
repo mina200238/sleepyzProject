779506d341bd032d9e85b865916957558e304234
9woody5 <woojin958@gmail.com>
2023-10-08 17:37:19 +0900

    styles: Update public header/footer styles

--- src/views/public/Footer/Footer.js
+++ src/views/public/Footer/Footer.js
@@ -1,5 +1,5 @@
 // footer 모듈 가져오기
-fetch('../../public/Footer/Footer.html')
+fetch('/src/views/public/Footer/Footer.html')
   .then((response) => response.text())
   .then((data) => {
     document.getElementById('footer').innerHTML = data;
