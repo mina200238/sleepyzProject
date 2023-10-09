604fd8abdffb9490ad366cab2366d50a2d86aa5b
[SW6] 김호진 <trr0373@gmail.com>
2023-10-09 22:29:33 +0900

    isAdmin

---
+++ src/routers/adminRouter.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+const { getAllProducts } = require('../controllers/productController');
+
+const router = Router();
+
+router.get('/products', getAllProducts); // 상품 정보 받아오기
+router.post('/products'); // 상품 추가
+router.put('/products'); // 상품 수정
+router.delete('/products'); // 상품 삭제
+
+router.get('/category'); // 카테고리 정보 받아오기
+router.post('/category'); // 카테고리 추가
+router.put('/category'); // 카테고리 수정
+router.delete('/category'); // 카테고리 삭제
+
+router.get('/orders'); // 주문 정보 받아오기
+router.post('/orders'); // 주문 추가
+router.put('/orders'); // 주문 수정
+router.delete('/orders'); // 주문 삭제
+
+module.exports = router;
---
+++ src/services/adminService.js
@@ -0,0 +1,65 @@
+const { User } = require('../models');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+class UserService {
+  // 회원 등록 확인
+  async checkRegistration(email) {
+    const isRegistered = await User.find({ email: email });
+    console.log(isRegistered);
+    if (isRegistered.length !== 0 && isRegistered[0].deleted_at) {
+      return undefined;
+    }
+
+    return isRegistered;
+  }
+
+  // 회원 등록
+  async signUp(name, email, password, phone_number, address) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = {
+      name,
+      email,
+      password: hashedPassword,
+      address,
+      phone_number,
+      admin_role: 0,
+    };
+    const user = await User.create(newUser);
+    return user;
+  }
+
+  // 로그인
+  async login(email, password) {
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const accessToken = jwt.sign(
+        {
+          user: {
+            username: user.name,
+            email: user.email,
+            id: user._id,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECERT,
+        { expiresIn: '50m' },
+      );
+      return accessToken;
+    } else return undefined;
+  }
+
+  // 회원 탈퇴
+  async signOut(decoded) {
+    const { id } = decoded.user;
+    const deletedUser = await User.findOneAndUpdate({ _id: id }, { deleted_at: new Date() });
+    return deletedUser;
+  }
+
+  // 회원 정보 수정
+  async updateUserInfo(email, name, phone_number, address) {
+    const updatedUserInfo = await User.updateMany({ email: email }, { name, phone_number, address });
+    return updatedUserInfo;
+  }
+}
+
+module.exports = UserService;
