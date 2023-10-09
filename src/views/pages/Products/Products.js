01c678a6684cfb8b945d8870b2fc39cc206e2b83
Namhyun <jenple9709@gmail.com>
2023-10-09 10:38:39 +0900

    refactor: Refactoring Main.css

--- src/views/pages/Products/Products.js
+++ src/views/pages/Products/Products.js
@@ -45,3 +45,77 @@ document.addEventListener('DOMContentLoaded', function () {
   updateView();
   updateURI();
 });
+
+//필터 구현해야 하는 코드(수정중)
+
+// let currentPage = 1;
+// let filteredProducts = [];
+
+// function updateURI() {
+//   const currentURL = window.location.href.split('?')[0];
+//   const newURL = `${currentURL}?page=${currentPage}`;
+//   window.history.pushState({ page: currentPage }, '', newURL);
+// }
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const itemsPerPage = 9;
+//   let products = Array.from(document.querySelectorAll('.product-card'));
+//   let totalPage = Math.ceil(products.length / itemsPerPage);
+//   let filteredProducts = Array.from(document.querySelectorAll('.product-card'));
+
+//   products.forEach((product) => (product.style.display = 'block'));
+
+//   function filterProducts(category) {
+//     if (category === '전체' || category === 'Products') {
+//       products.forEach((product) => (product.style.display = 'block'));
+//     } else {
+//       products.forEach((product) => {
+//         if (product.getAttribute('data-category') === category) {
+//           product.style.display = 'block';
+//         } else {
+//           product.style.display = 'none';
+//         }
+//       });
+//     }
+//     filteredProducts = products.filter((product) => product.style.display === 'block');
+
+//     currentPage = 1;
+//     totalPage = Math.ceil(filteredProducts.length / itemsPerPage);
+//     updateView();
+//     updateURI();
+//   }
+//   function updateView() {
+//     products.forEach((product) => (product.style.display = 'none'));
+
+//     const startIndex = (currentPage - 1) * itemsPerPage;
+//     const endIndex = startIndex + itemsPerPage;
+//     filteredProducts.slice(startIndex, endIndex).forEach((product) => {
+//       product.style.display = 'block';
+//     });
+//   }
+
+//   document.querySelector('.pagination-bar').addEventListener('click', function (e) {
+//     if (e.target.classList.contains('link')) {
+//       currentPage = parseInt(e.target.value);
+//     } else if (e.target.classList.contains('prev-btn')) {
+//       if (currentPage > 1) currentPage--;
+//     } else if (e.target.classList.contains('next-btn')) {
+//       if (currentPage < totalPage) currentPage++;
+//     }
+//     updateView();
+//     updateURI();
+//   });
+
+//   const categories = document.querySelectorAll('.category-item a');
+
+//   categories.forEach((category) => {
+//     category.addEventListener('click', function (event) {
+//       event.preventDefault();
+
+//       let currentCategory = this.getAttribute('href').substring(1);
+//       filterProducts(currentCategory);
+//     });
+//   });
+
+//   filterProducts('전체');
+// });
