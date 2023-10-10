36d96f601fa547297095a31204442486a938dc67
[SW6] 김호진 <trr0373@gmail.com>
2023-10-10 15:03:17 +0900

    getCategories fix

--- src/models/schemas/order.js
+++ src/models/schemas/order.js
@@ -4,7 +4,7 @@ const { Schema } = require('mongoose');
 const OrderSchema = new Schema(
   {
     user_id: {
-      type: Schema.Types.ObjectId,
+      type: String,
       required: true,
     },
     products_id: {
