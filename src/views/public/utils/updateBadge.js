8e1f531089739d4d2945e96b9f00d8b8a0ab8ad9
9woody5 <woojin958@gmail.com>
2023-10-12 05:58:51 +0900

    fix: Update Cart page

--- src/views/public/utils/updateBadge.js
+++ src/views/public/utils/updateBadge.js
@@ -1,18 +1,15 @@
 // 장바구니 아이콘 badge 숫자 업데이트
 export default function updateBadge() {
   const badgeElement = document.getElementById('cart-badge');
-  console.log('updateBadge function called');
-  console.log(badgeElement);
+
   if (badgeElement) {
     // 로컬 스토리지에서 key들을 모두 가져옴
     const keys = Object.keys(localStorage);
 
     // "product_"로 시작하는 key들만 필터링
     const productKeys = keys.filter((key) => key.startsWith('product_'));
-    console.log(productKeys);
 
     // "product_"로 시작하는 key들의 개수를 사용하여 뱃지 아이콘 업데이트
     badgeElement.textContent = productKeys.length.toString();
-    console.log(productKeys.length.toString());
   }
 }
