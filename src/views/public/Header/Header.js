6eae4805aabda6b7eeb3ceaf461929543a27a640
9woody5 <woojin958@gmail.com>
2023-10-11 21:35:08 +0900

    feat: Add favicon and Update badge func

--- src/views/public/Header/Header.js
+++ src/views/public/Header/Header.js
@@ -1,16 +1,12 @@
 import updateBadge from '/public/utils/UpdateBadge.js';
 
-// 페이지가 로드될 때 실행
-window.addEventListener('load', () => {
-  fetchHeader();
-  updateBadge();
-});
-
 async function fetchHeader() {
   try {
     const response = await fetch('/public/Header/Header.html');
     const data = await response.text();
     document.getElementById('header').innerHTML = data;
+
+    updateBadge();
 
     const categoryLinks = document.querySelectorAll('.dropdown a');
 
@@ -25,3 +21,26 @@ async function fetchHeader() {
     console.error('헤더를 가져오는 중 오류가 발생했습니다:', error);
   }
 }
+
+async function addFavicon() {
+  const head = document.querySelector('head');
+  // SVG 파비콘 추가
+  const faviconSVG = document.createElement('link');
+  faviconSVG.rel = 'icon';
+  faviconSVG.type = 'image/svg+xml';
+  faviconSVG.href = '/public/assets/favicon.svg';
+  head.appendChild(faviconSVG);
+
+  // PNG 파비콘 추가
+  const faviconPNG = document.createElement('link');
+  faviconPNG.rel = 'icon';
+  faviconPNG.type = 'image/png';
+  faviconPNG.href = '/public/assets/favicon.png';
+  head.appendChild(faviconPNG);
+}
+
+// 페이지가 로드될 때 실행
+window.addEventListener('DOMContentLoaded', () => {
+  addFavicon();
+  fetchHeader();
+});
