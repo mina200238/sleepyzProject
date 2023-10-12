b3567884ac2431243a3c1e7c445da00a46c10285
김승아 <ksa7841@gmail.com>
2023-10-12 21:01:11 +0900

    refactor: Order-History

--- src/views/pages/Order-History/Order-History.js
+++ src/views/pages/Order-History/Order-History.js
@@ -6,10 +6,14 @@ document.addEventListener('DOMContentLoaded', async () => {
   if (userEmail) {
     try {
       const orderDetails = await fetchOrderDetails(userEmail);
-      // 주문 정보를 화면에 표시하는 코드 추가
-      //   for (let i = 0; i < orderDetails.length; i++) {
-      //     displayOrderDetails(orderDetails[i]);
-      //   }
+      const container = document.querySelector('.order-container');
+
+      const products = orderDetails[0].products_id;
+      const productskeysArray = Object.keys(products);
+      console.log(`productskeysArray: ${productskeysArray}`);
+      const productId = productskeysArray[0];
+      const response = await axios.get(`http://localhost:5000/products/6524bc616e44b087ce217c8e`);
+      console.log(response.data);
     } catch (error) {
       console.error('Error fetching or displaying order details:', error);
     }
@@ -19,23 +23,10 @@ document.addEventListener('DOMContentLoaded', async () => {
 async function fetchOrderDetails(email) {
   try {
     const response = await axios.get(`http://localhost:5000/orders/search?email=${email}`);
-    // console.log(response.data.data[0]);
-    const products = response.data.data[0].products_id;
-    const productskeysArray = Object.keys(products); //상품id 배열
-    const firstProperty = products[productskeysArray[0]]; //첫번째 상품id 갯수
-    console.log(response.data.data[0]);
-    console.log(productskeysArray); //상품id 배열
-    console.log(firstProperty); //첫번째 상품id 갯수
+
     return response.data.data;
   } catch (error) {
     console.error('Error fetching order details:', error);
     throw error;
   }
-}
-
-function displayOrderDetails(order) {
-  const container = document.querySelector('.order-container');
-
-  const products = order.products_id;
-  const productskeysArray = Object.keys(products); //상품ID 배열
 }
