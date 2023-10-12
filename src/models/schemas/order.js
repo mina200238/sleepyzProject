dfaf1af52026db1c23769fa505e72b0a79895ec0
[SW6] 김호진 <trr0373@gmail.com>
2023-10-12 20:52:35 +0900

    orderAPI fix

--- src/models/schemas/order.js
+++ src/models/schemas/order.js
@@ -1,14 +1,17 @@
 /* eslint-disable prettier/prettier */
 const { Schema } = require('mongoose');
 
+// [상품id, 상품명, 상품가격, 수량]
+// [상품id, 상품명, 상품가격, 수량]
+
 const OrderSchema = new Schema(
   {
     user_id: {
       type: String,
       required: true,
     },
-    products_id: {
-      type: Object,
+    products: {
+      type: Array,
       required: true,
     },
     name: {
