48288de87446f06a68767bdc2131a284d3e85e40
[SW6] 김호진 <trr0373@gmail.com>
2023-10-14 02:09:11 +0900

    delete

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -186,7 +186,7 @@ const deleteOrders = async (req, res, next) => {
     const adminService = new AdminService();
     const updatedUserInfo = await adminService.deleteOrders(order_id);
     res.status(200).json({
-      data: nuupdatedUserInfoll,
+      data: updatedUserInfo,
       id: order_id,
       message: '주문 내역이 성공적으로 삭제되었습니다',
     });
