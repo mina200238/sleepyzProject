7b6d411597613d97ebb3836658d693bd227298cb
[SW6] 김호진 <trr0373@gmail.com>
2023-10-13 16:38:45 +0900

    router fix2

--- src/services/viewService.js
+++ src/services/viewService.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const ViewService = {
   serveStatic: (resource) => {
-    const pathName = path.join(__dirname, `/pages/${resource}`);
+    const pathName = path.join(__dirname, `./views/pages/${resource}`);
     const option = { index: `${resource}.html` };
     return express.static(pathName, option);
   },
