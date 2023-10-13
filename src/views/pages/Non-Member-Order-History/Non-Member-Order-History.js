b16d7bb9c4ae40e7c67a780fa66de64dd88e0a2f
[SW6] 김호진 <trr0373@gmail.com>
2023-10-14 02:35:47 +0900

    delete

--- src/views/pages/Non-Member-Order-History/Non-Member-Order-History.js
+++ src/views/pages/Non-Member-Order-History/Non-Member-Order-History.js
@@ -136,10 +136,11 @@ function displayOrderDetails(orderDetails) {
       });
       orderDeleteBtn.addEventListener('click', async function () {
         console.log('delte버튼 클릭!', orderInfoList._id);
+        const order_id = orderInfoList._id;
         try {
           const response = await axios.post(`${BASE_URL}/orders/delete`, {
             headers: {
-              order_id: orderInfoList._id,
+              order_id: order_id,
             },
           });
           if (response.status === 200) {
