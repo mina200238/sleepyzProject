1e17ea5abea45c706f89ed0822ecc3d85bf7cfbd
9woody5 <woojin958@gmail.com>
2023-10-13 15:59:59 +0900

    refactor: Delete Token Func

--- src/views/public/Header/Header.js
+++ src/views/public/Header/Header.js
@@ -116,24 +116,6 @@ function getAccessTokenFromCookie() {
   return null;
 }
 
-// 쿠키에서 accessToken을 가져오는 함수
-function getAccessTokenFromCookie() {
-  const name = 'accessToken=';
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookieArray = decodedCookie.split(';');
-
-  for (let i = 0; i < cookieArray.length; i++) {
-    let cookie = cookieArray[i];
-    while (cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1);
-    }
-    if (cookie.indexOf(name) === 0) {
-      return cookie.substring(name.length, cookie.length);
-    }
-  }
-  return null;
-}
-
 function checkUserLoginStatus() {
   const token = getAccessTokenFromCookie();
   if (!token) {
